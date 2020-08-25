129d44bb6dcb38a459bc193c24ea56ac929860bb
Kauanphbbb <kauanphbbb@gmail.com>
2020-08-25 18:29:33 -0300

    Adicionando Jquery para o menu abrir e fechar

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,6 @@
-console.log("funcionou");
+$(document).ready(function () {
+  const btnMenuMobile = $(".btn-menumobile");
+  $(btnMenuMobile).on("click", function () {
+    $(".nav-container ul").toggleClass("open");
+  });
+});
